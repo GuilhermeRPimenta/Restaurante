c667d47af7e1415370bcc3815cb4518d13041b1f
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-11-17 14:37:03 -0300

    feat(client): adds favicon

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className=" bg-gray-100 shadow-md flex items-center p-1 justify-center">
       <img src={logoOrange} alt="" className="w-12" />
-      <h1 className="font-pacifico text-3xl text-primary">Prato Bom</h1>
+      <h1 className="font-pacifico text-3xl text-primary">Bom Prato</h1>
     </header>
   );
 };
