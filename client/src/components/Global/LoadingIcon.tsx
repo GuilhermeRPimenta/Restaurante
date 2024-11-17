57ddc930a2c08ff384812bb251086d68312b0c49
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-11-17 19:06:55 -0300

    feat(client): adds LoadingIcon

---
+++ client/src/components/Global/LoadingIcon.tsx
@@ -0,0 +1,11 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+const LoadingIcon = () => {
+  return (
+    <div>
+      <AiOutlineLoading3Quarters className="animate-spin" />
+    </div>
+  );
+};
+
+export default LoadingIcon;
