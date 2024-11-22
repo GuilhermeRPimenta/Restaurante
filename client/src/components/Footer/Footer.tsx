de2346aedb679c81837ada886aac90f18dff63c6
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-11-21 23:49:48 -0300

    feat(client): adds number of ordered items in footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,11 +5,15 @@ import { useCart } from "../Global/CartProvider";
 const Footer = () => {
   const location = useLocation();
   const cart = useCart();
-  const total = cart.cartContent.reduce((acc, product) => {
+  const { cartContent } = cart;
+  const total = cartContent.reduce((acc, product) => {
     const val = Number(product.price) * product.quantity;
     return acc + val;
   }, 0);
   const hiddenFooter = total === 0;
+  const numberOfItems = cartContent.reduce((acc, content) => {
+    return acc + content.quantity;
+  }, 0);
   return (
     <footer
       className={`bg-gray-100 p-2 flex gap-2 items-center justify-center fixed z-[10] w-full transition-all duration-200 ${
@@ -20,9 +24,12 @@ const Footer = () => {
           "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -2px rgba(0, 0, 0, 0.1)",
       }}
     >
-      <div className="font-bold mx-auto ">{`Total: R$${total
-        .toFixed(2)
-        .replace(".", ",")}`}</div>
+      <div className="font-bold mx-auto ">
+        <p>{`${numberOfItems} ${numberOfItems === 1 ? "item" : "itens"}`}</p>
+        <p className="whitespace-nowrap">{`Total: R$ ${total
+          .toFixed(2)
+          .replace(".", ",")}`}</p>
+      </div>
       {location.pathname === "/" && (
         <ButtonLink className="px-8 md:px-32 mx-auto " to="/checkout">
           Finalizar
